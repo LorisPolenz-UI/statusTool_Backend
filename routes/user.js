f9db04d06f65d81f2937b63f25dadda775793d8a
LorisPolenz-UI <59690885+LorisPolenz-UI@users.noreply.github.com>
2020-12-04 21:58:48 +0100

    stuff done

--- routes/user.js
+++ routes/user.js
@@ -4,55 +4,95 @@ const Sequelize = require('sequelize');
 
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { contains } = require('sequelize/types/lib/operators');
 
-module.exports.signup = (req, res) =>
-    user.findOne({
-        where: {
-            email: req.body.email
-        }
-    })
-        .then(element => {
-            if (element !== null) {
-                console.log('Email already exists');
-                return res.status(409).send({
-                    message: 'Email already exists'
-                })
-            } else {
-                bcrypt.hash(req.body.password, 10, (err, hash) => {
-                    if (err) {
-                        return res.status(500);
-                        console.log(err);
-                    } else {
-                        user.create({
-                            email: req.body.email,
-                            password: hash,
-                            createdAt: new Date(),
-                            updatedAt: new Date()
-                        })
-                            .then(crstatus => {
-                                res.status(200).send({
-                                    message: 'You are now sign up'
-                                })
-                            })
-                            .catch(err => {
-                                console.log(err)
-                                return res.status(400).json({
-                                    message: 'An error occured!',
-                                    error: err
-                                })
-                            })
-                    }
-                });
-            }
-        })
-        .catch(err => {
-            console.log(err)
-            return res.status(400).json({
-                message: 'An error occured!',
-                error: err
+
+
+
+function ValidateEmail(mail) {
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail)) {
+        console.log("Valid E-Mail entered");
+        return true;
+    } else {
+        console.log("Not valid E-Mail entered");
+        return false;
+    }
+}
+
+function ValidatePassword(password) {
+
+}
+
+function ValidateInput (mail, password) {
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail)) {
+        console.log(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail))
+        return true;
+    } else if (password.length > 10) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+
+module.exports.signup = (req, res) => {
+    if (ValidateInput(req.body.mail, req.body.password) != true) {
+            res.status(300).send({
+                message: "E-Mail or password are invalid"
             })
-        });
+        }else {
+            user.findOne({
+                where: {
+                    email: req.body.email
+                }
+            })
+                .then(element => {
+                    if (element !== null) {
+                        console.log('Email already exists');
+                        return res.status(409).send({
+                            message: 'Email already exists'
+                        })
+                    } else {
+                        bcrypt.hash(req.body.password, 10, (err, hash) => {
+                            if (err) {
+                                return res.status(500);
+                                console.log(err);
+                            } else {
+                                user.create({
+                                    email: req.body.email,
+                                    password: hash,
+                                    createdAt: new Date(),
+                                    updatedAt: new Date()
+                                })
+                                    .then(crstatus => {
+                                        res.status(200).send({
+                                            message: 'You are now sign up'
+                                        })
+                                    })
+                                    .catch(err => {
+                                        console.log(err)
+                                        return res.status(400).json({
+                                            message: 'An error occured!',
+                                            error: err
+                                        })
+                                    })
+                            }
+                        });
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                    return res.status(400).json({
+                        message: 'An error occured!',
+                        error: err
+                    })
+                });
+
+        }
+    }
+
+
 
 
 
